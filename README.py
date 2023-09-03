f61b8fe0cac53da659a31d7e71b5810213c4b4b5
messi <66263120+Kooper77@users.noreply.github.com>
2023-09-03 13:57:50 +0100

    Update README.py

--- README.py
+++ README.py
@@ -1,15 +1,15 @@
-#Kovex
+#𝙆𝙤𝙫𝙚𝙭
 
-SISTEMAS SUPORTADOS:
+𝙎𝙄𝙎𝙏𝙀𝙈𝘼𝙎 𝙎𝙐𝙋𝙊𝙍𝙏𝘼𝘿𝙊𝙎:
 
-●TERMUX ●LINUX
+●𝙏𝙀𝙍𝙈𝙐𝙓 ●𝙇𝙄𝙉𝙐𝙓
 
-INSTALAÇÃO:
+𝙄𝙉𝙎𝙏𝘼𝙇𝘼𝘾̧𝘼̃𝙊:
 
-apt update
+𝙖𝙥𝙩 𝙪𝙥𝙙𝙖𝙩𝙚
 
-apt upgrade
+𝙖𝙥𝙩 𝙪𝙥𝙜𝙧𝙖𝙙𝙚
 
-git clone https://github.com/Kooper77/Kovex
+𝙜𝙞𝙩 𝙘𝙡𝙤𝙣𝙚 𝙝𝙩𝙩𝙥𝙨://𝙜𝙞𝙩𝙝𝙪𝙗.𝙘𝙤𝙢/𝙆𝙤𝙤𝙥𝙚𝙧𝟳𝟳/𝙆𝙤𝙫𝙚𝙭
 
-python Kovex.py
+𝙥𝙮𝙩𝙝𝙤𝙣 𝙆𝙤𝙫𝙚𝙭.𝙥𝙮
